34198cb9a3bb429f0322491d93f94cb33e01f320
Camila Gallegos <camigm1@gmail.com>
2022-07-29 13:32:55 -0600

    changes

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(num) {
+  return function (plusNum) {
+    // console.log(plus15(10));
+    return plusNum + num;
+  };
+}
+let plus15 = plus(15);
+console.log(plus15(10));
